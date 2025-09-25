3c719eb6d8ce03487ce6cebc4bda15ee3d4ab611
OlDeuS <66070083@kmitl.ac.th>
2025-09-25 22:45:27 +0700

    start project

---
+++ Borrow_Dee/Borrow_Dee/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Borrow_Dee project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Borrow_Dee.settings')
+
+application = get_asgi_application()
