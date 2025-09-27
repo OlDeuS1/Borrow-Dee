f29237ad120c5d6d7f6410f784f15f88c1bde68b
OlDeuS <66070083@kmitl.ac.th>
2025-09-27 14:00:07 +0700

    add library URLs and create index view with template

--- Borrow_Dee/library/views.py
+++ Borrow_Dee/library/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from .models import *
+from django.db.models import *
+from django.db.models.functions import *
+from django.shortcuts import redirect, get_object_or_404
+# from .forms import *
+from django.db import transaction
 
-# Create your views here.
+class IndexView(View):
+
+    def get(self, request):
+
+        return render(request, "index.html")
